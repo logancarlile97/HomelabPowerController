2d8aa11a3cc758c0a0c7ec8a88c26b5aaa789c71
Logan Carlile <logancarlile97@gmail.com>
2021-06-21 21:14:54 -0400

    Update lcd_driver.py
    Added logging to the lcd driver also added some bug fixes

--- lcd_driver.py
+++ lcd_driver.py
@@ -2,13 +2,12 @@ import adafruit_character_lcd.character_lcd as characterLCD
 import board
 import digitalio
 import time
-
+import logging
 
 class LcdDriver():
     """
     A class to handle the lcd
     """
-
     def __init__(self):
         # Assign each lcd pin to GPIO
         lcd_rs = digitalio.DigitalInOut(board.D17)  # GPIO 17 pin 11
@@ -22,8 +21,23 @@ class LcdDriver():
 
         self.lcd = characterLCD.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns,
                                                    lcd_rows)
+        #Create and configure logger
+        LOG_Format = "%(levelname)s %(asctime)s - %(message)s"
+        logging.basicConfig(filename = 'test.log',
+                            level = logging.INFO,
+                            format = LOG_Format)
 
-    def message(self, top, bottom):
+        self.logger = logging.getLogger()
+
+    def print(self, top, bottom):
+        """
+        Prints top and bottom to the top and bottom of the lcd respectivly
+        """
+        log = self.logger
+
+        log.debug(f'lcd top message: {top}')
+        log.debug(f'lcd bottom message: {bottom}')
+        time.sleep(1) #Delay to allow any previous lcd message to be read by user
         if len(top) == 0:
             adjTop = top
         else:
@@ -34,8 +48,19 @@ class LcdDriver():
         else:
             adjBottom = "\n" + bottom.ljust(16)
 
+        log.debug(f'lcd message printed')
         self.lcd.message = adjTop + adjBottom
-        time.sleep(0.5)
+        log.info(f'Current lcd message {adjTop} \t{adjBottom}')
 
     def clear(self):
+        """
+        Clears all text on the lcd
+        """
+        log = self.logger
+
+        
         self.lcd.clear()
+        log.info(f'LCD has been cleared')
+
+
+lcd = LcdDriver()
