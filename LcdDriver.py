12978d0cd4950708b1cbbf86c8e8f9c664d9d347
Logan Carlile <logancarlile97@gmail.com>
2021-06-28 03:02:10 -0400

    Update LcdDriver.py

--- LcdDriver.py
+++ LcdDriver.py
@@ -44,12 +44,14 @@ class LcdDriver():
             adjTop = top
         else:
             top = top.strip()
+            top = top[0:15]
             adjTop = top.ljust(16)
 
         if len(bottom) == 0:
             adjBottom = bottom
         else:
             bottom = bottom.strip()
+            bottom = bottom[0:15]
             adjBottom = "\n" + bottom.ljust(16)
 
         log.debug(f'lcd message printed')
